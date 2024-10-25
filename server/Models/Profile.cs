0e7fdd8b1a9793753c61cd901c05aa64edbc2098
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-25 12:13:51 -0600

    Added coverimg to profile model

--- server/Models/Profile.cs
+++ server/Models/Profile.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using keepr.interfaces;
 
 namespace keepr.Models;
@@ -7,6 +8,8 @@ public class Profile : IRepoItem<string>
   public string Id { get; set; }
   public string Name { get; set; }
   public string Picture { get; set; }
+  [MaxLength(1000)]
+  public string CoverImg { get; set; }
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
 }

0e855276c57a6ab3d25be3231ff32ab28902c583
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-25 10:46:21 -0600

    init

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'dev-yptxrrc8jmvr0wmz.us.auth0.com'
+export const clientId = 'wT1PhGZYQz85SXv9SA4TnybT4ov26x6B'
+export const audience = 'https://auth.speedwagonfoundation.com'

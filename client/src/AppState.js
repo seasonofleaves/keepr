39e2589f77e3cbee8cd3f6f2f1a8277602d780da
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-25 22:34:33 -0600

    Added keep properties to appstate

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -5,6 +5,10 @@ export const AppState = reactive({
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
   /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null
+  account: null,
+  /**@type {import('./models/Keep.js').Keep[]} */
+  keeps: [],
+  /**@type {import('./models/Keep.js').Keep} */
+  keep: null,
 })
 

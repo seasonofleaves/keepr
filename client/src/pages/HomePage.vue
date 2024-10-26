296d0ea8a0c1267d1453cbaf2fcfd3d78a9dff80
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-25 23:21:02 -0600

    Raw data dumping keeps to the home page

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,8 +1,11 @@
 <script setup>
+import { AppState } from '@/AppState.js';
 import { keepsService } from '@/services/KeepsService.js';
 import { logger } from '@/utils/Logger.js';
 import Pop from '@/utils/Pop.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
+
+const keeps = computed(() => AppState.keeps)
 
 onMounted(() => {
   getAllKeeps()
@@ -21,14 +24,12 @@ async function getAllKeeps(){
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container-fluid">
+    <section class="row">
+      <div v-for="keep in keeps" :key="keep.id" class="col-12">
+        {{ keep }}
+      </div>
+    </section>
   </div>
 </template>
 

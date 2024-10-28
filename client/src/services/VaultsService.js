b925c51c7d0cc3e4261b1fc88ad12bb2746adfe8
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-27 20:57:05 -0600

    Added pop notification when creating vault

--- client/src/services/VaultsService.js
+++ client/src/services/VaultsService.js
@@ -2,6 +2,7 @@ import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
 import { Vault } from "@/models/Vault.js"
 import { AppState } from "@/AppState.js"
+import Pop from "@/utils/Pop.js"
 
 class VaultsService{
   async createVault(vaultData) {
@@ -9,6 +10,7 @@ class VaultsService{
     logger.log('Created vault', response.data)
     const newVault = new Vault(response.data)
     AppState.vaults.push(newVault)
+    Pop.success(`You created a new vault!`)
   }
 
 }

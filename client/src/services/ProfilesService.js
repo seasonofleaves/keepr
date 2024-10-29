4061613bd3fc6808b95625b73dcc29c910c1cf86
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-29 09:57:32 -0600

    Accessing value of route parameter and logging profile to console - created profile service

---
+++ client/src/services/ProfilesService.js
@@ -0,0 +1,12 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+class ProfilesService{
+  async getProfileById(profileId) {
+    const response = await api.get(`api/profiles/${profileId}`)
+    logger.log('Got profile - profiles service', response.data)
+  }
+
+}
+
+export const profilesService = new ProfilesService()

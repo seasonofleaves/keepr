ed4799b1fdb3dd56e39d5005a6b90db463b318d1
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-29 10:04:24 -0600

    Clearing ghost data out of Appstate

--- client/src/services/ProfilesService.js
+++ client/src/services/ProfilesService.js
@@ -5,6 +5,7 @@ import { AppState } from "@/AppState.js"
 
 class ProfilesService{
   async getProfileById(profileId) {
+    AppState.activeProfile = null
     const response = await api.get(`api/profiles/${profileId}`)
     // logger.log('Got profile - profiles service', response.data)
     const newProfile = new Profile(response.data)

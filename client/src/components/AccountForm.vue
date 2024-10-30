dbd192bee151cee845441a5d459cf8a5aff0d998
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-30 12:40:10 -0600

    User can update their account

--- client/src/components/AccountForm.vue
+++ client/src/components/AccountForm.vue
@@ -1,35 +1,59 @@
 <script setup>
+import { AppState } from '@/AppState.js';
+import { accountService } from '@/services/AccountService.js';
+import { logger } from '@/utils/Logger.js';
+import Pop from '@/utils/Pop.js';
+import { onMounted, ref } from 'vue';
+
+const editableProfileData = ref({
+  name: '',
+  picture: '',
+  coverImg: ''
+})
+
+onMounted(() => {
+  editableProfileData.value = { ...AppState.account }
+})
+
+async function updateAccount() {
+  try {
+    await accountService.updateAccount(editableProfileData.value)
+    Pop.success("Changes saved to your account!")
+  }
+  catch (error) {
+    Pop.error(error)
+    logger.log(error)
+  }
+}
 
 </script>
 
 
 <template>
-  <!-- updateAccount() -->
-
   <div class="container">
     <section class="row g-3">
       <div class="col-12">
         <h1>Update your profile</h1>
-        <form @submit.prevent="">
+        <form @submit.prevent="updateAccount()">
           <div class="form-group mt-4">
-            <input type="text" class="form-control" id="name" placeholder="Name..." minlength="5" maxlength="30"
-              required>
+            <input v-model="editableProfileData.name" type="text" class="form-control" id="accountName"
+              placeholder="Name..." minlength="5" maxlength="30" required>
           </div>
           <div class="form-group mt-2">
-            <input type="url" class="form-control" id="profilePictureUrl" placeholder="Profile Picture URL..."
-              minlength="100" maxlength="1000" required>
+            <input v-model="editableProfileData.picture" type="url" class="form-control" id="profilePictureUrl"
+              placeholder="Profile Picture URL..." minlength="60" maxlength="1000" required>
           </div>
           <div class="form-group mt-2">
-            <input type="url" class="form-control" id="coverImgUrl" placeholder="Cover Image URL..." minlength="100"
-              maxlength="1000" required>
+            <input v-model="editableProfileData.coverImg" type="url" class="form-control" id="coverImgUrl"
+              placeholder="Cover Image URL..." minlength="100" maxlength="1000" required>
           </div>
-          //TODO - Add bio?
+          <!-- //TODO - Add bio? -->
           <!-- <div class="form-group mt-2">
             <textarea  rows="8" type="text" class="form-control" id="bio"
               placeholder="Your bio..." minlength="5" maxlength="1000"></textarea>
           </div> -->
           <div class="text-end mt-2">
-            <button type="submit" class="btn btn-dark">Create</button>
+            <button type="submit" class="btn btn-dark">Update</button>
           </div>
         </form>
       </div>
--- client/src/services/AccountService.js
+++ client/src/services/AccountService.js
@@ -4,6 +4,13 @@ import { logger } from '../utils/Logger.js'
 import { api } from './AxiosService.js'
 
 class AccountService {
+  async updateAccount(accountData) {
+    const response = await api.put('/account', accountData)
+    logger.log('Updated account - account service', response.data)
+    const newAccount = new Account(response.data)
+    AppState.account = newAccount
+  }
+
   async getAccount() {
     try {
       const res = await api.get('/account')

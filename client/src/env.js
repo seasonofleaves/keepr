79aa85c34638c912d3553de63164a7b6d3ccd2ce
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-31 11:32:31 -0600

    deply please - auth0 not working

--- client/src/env.js
+++ client/src/env.js
@@ -2,5 +2,5 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-yptxrrc8jmvr0wmz.us.auth0.com'
-export const clientId = 'wT1PhGZYQz85SXv9SA4TnybT4ov26x6B'
+export const clientId = 'v6rEtUdK5Cnm4LNOeEiXqq8qUScUT15I'
 export const audience = 'https://auth.speedwagonfoundation.com'
